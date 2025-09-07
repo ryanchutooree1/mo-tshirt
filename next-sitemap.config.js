422229cc8eb899af8a947f961eb97060ba72d7fa
Ryan Chutooree <ryanchutooree@gmail.com>
2025-09-08 00:43:39 +0400

    Use canonical domain as sitemap fallback (mo-tshirt.mu)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 const siteUrl =
   process.env.SITE_URL ||
   process.env.NEXT_PUBLIC_SITE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
+  "https://mo-tshirt.mu";
 
 module.exports = {
   siteUrl,
