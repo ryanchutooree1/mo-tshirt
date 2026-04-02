e46e1ba9836d863219c418cc3f874e0c43c71e48
Ryan Chutooree <ryanchutooree@gmail.com>
2026-04-02 21:17:04 +0400

    Relax Android NDK pin

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "mu.motshirt.mo_tshirt_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
